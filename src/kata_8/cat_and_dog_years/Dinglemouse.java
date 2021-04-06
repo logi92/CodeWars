b811f761fc0e4478c02b1ca8750588a365f73309
Ruslan Nazar <r.nazar92@mail.ru>
2021-04-06 09:58:52 +0300

    resolve Cat and Dogs Years

---
+++ src/kata_8/cat_and_dog_years/Dinglemouse.java
@@ -0,0 +1,46 @@
+package kata_8.cat_and_dog_years;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/*
+https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+ */
+public class Dinglemouse {
+    public static void main(String[] args) {
+        exampleTests();
+    }
+
+    public static int[] humanYearsCatYearsDogYears(final int humanYears) {
+        int catYears = 0;
+        int dogYears = 0;
+
+        for (int i = 1; i <= humanYears; i++) {
+            if (i == 1) {
+                catYears += 15;
+                dogYears += 15;
+            } else if (i == 2) {
+                catYears += 9;
+                dogYears += 9;
+            } else {
+                catYears += 4;
+                dogYears += 5;
+            }
+        }
+        return new int[]{humanYears, catYears, dogYears};
+    }
+
+    public static void exampleTests() {
+        assertArrayEquals(new int[]{1, 15, 15}, Dinglemouse.humanYearsCatYearsDogYears(1));
+        assertArrayEquals(new int[]{2, 24, 24}, Dinglemouse.humanYearsCatYearsDogYears(2));
+        assertArrayEquals(new int[]{10, 56, 64}, Dinglemouse.humanYearsCatYearsDogYears(10));
+    }
+}
